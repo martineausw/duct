d37b89cfc4d14b2af7720ccf4e40b57c50f781ec
Seth Martineau <martineausw@gmail.com>
2025-08-27 00:57:36 -0400

    initial commit

---
+++ build.zig
@@ -0,0 +1,31 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+
+    const mod = b.addModule("duct", .{
+        .root_source_file = b.path("src/root.zig"),
+        .target = target,
+    });
+
+    const ziggurat = b.dependency("ziggurat", .{
+        .target = target,
+    });
+
+    mod.addImport("ziggurat", ziggurat.module("ziggurat"));
+
+    const mod_tests = b.addTest(.{
+        .root_module = mod,
+    });
+
+    const ziggurat_tests = b.addTest(.{
+        .root_module = ziggurat.module("ziggurat"),
+    });
+
+    const run_mod_tests = b.addRunArtifact(mod_tests);
+    const run_ziggurat_tests = b.addRunArtifact(ziggurat_tests);
+
+    const test_step = b.step("test", "Run tests");
+    test_step.dependOn(&run_mod_tests.step);
+    test_step.dependOn(&run_ziggurat_tests.step);
+}
